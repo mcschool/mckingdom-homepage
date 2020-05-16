0c2c218ebcb6309afe49ea3e28b38fb056e4605e
Yuki-Shibanuma <smatosan@gmail.com>
2020-05-16 12:22:27 +0900

    a

---
+++ components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="head">
+    <div class="title">
+      MC-KINGDOM
+    </div>
+    <div class="subtitle">
+      MC-KINGDOM のホームページへようこそ！
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+.head{
+  width: 100%;
+  height: 150px;
+  background-image: linear-gradient(to right, #000000, #1a1419, #25232f, #273547, #1c495b, #1c495b, #1c495b, #1c495b, #273547, #25232f, #1a1419, #000000);
+}
+
+.title{
+  color: #D1E8E2;
+  text-shadow: 2px 2px #116466;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 65px;
+}
+.subtitle{
+  color: #D1E8E2;
+  text-shadow: 2px 2px #116466;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 25px;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,100 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        mckingdom-homepage
-      </h1>
-      <h2 class="subtitle">
-        My pioneering Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="full">
+    <Header></Header>
+
+    <div class="info">
+
+      <div class="game">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+            </figure>
+          </div>
+
+          <div class="content">
+            <strong>PvP: Player vs Player</strong> <br></br>
+            プレーヤー同士でバトル！ <br></br>
+            リンク＝＝＝＞<a href="http://localhost:3000/pvp">PvP</a>
+
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+            </figure>
+          </div>
+
+          <div class="content">
+            <strong>PvE: Player vs Enemy</strong> <br></br>
+            みんなで協力してモンスターを倒せ！ <br></br>
+            リンク＝＝＝＞<a href="http://localhost:3000/pvp">PvE</a>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+            </figure>
+          </div>
+
+          <div class="content">
+            <strong>アスレ: アスレッチック</strong> <br></br>
+            激ムズコースが10コース以上！<br></br>
+            リンク＝＝＝＞<a href="http://localhost:3000/pvp">アスレ</a>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+            </figure>
+          </div>
+
+          <div class="content">
+            <strong>生活サーバー: お金をためて生活しよう</strong> <br></br>
+            みんなでのんびりマイクラ生活！<br></br>
+            リンク＝＝＝＞<a href="http://localhost:3000/pvp">生活 </a>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Header from "~/components/Header"
 export default {
-  components: {
-    Logo
-  }
+  name: "Index",
+  components: { Header },
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+
+.info{
+  width: 100%;
+  background-image: linear-gradient(to right, #000000, #1a1419, #25232f, #273547, #1c495b, #1c495b, #1c495b, #1c495b, #273547, #25232f, #1a1419, #000000);
+}
+
+.card{
+  width: 18%;
+  display: inline-block;
+  margin: 14px;
+  padding: 1px;
+  border-radius: 3px;
+}
+
+.game{
+  display: inline-block;
+  width: 100%;
+}
+
+.content{
   text-align: center;
+  font-size: 15px;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
